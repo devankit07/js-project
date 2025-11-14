0a18a96fac5c21fd6deb641ceff1173d31d241a8
Ankitkumar277 <Ankitrathor272005@gmail.com>
2025-11-14 15:46:41 +0530

    done

---
+++ passgenerator/script.js
@@ -0,0 +1,23 @@
+const random =document.querySelector('#random');
+const btun = document.querySelector('.btn');
+const copy =document.querySelector('.cpy');
+
+
+btun.addEventListener('click',function(){
+
+    const chars= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%&";
+    let pass = "";
+    let lenght = 15 ;
+
+    for(let i=0;i<lenght; i++){
+        let randomgen =Math.floor(Math.random()*chars.length);
+        pass+= chars[randomgen];
+    }
+    return random.innerHTML= pass;
+});
+
+copy.addEventListener('click', ()=>{
+    navigator.clipboard.writeText(random.innerHTML)
+    alert('pass copied')
+})
+
